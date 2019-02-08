f89019eb2d82b9a25d3802a1c715de1da113d9fe
Priya Chaitanya Yadav <priyachaitanya.yadav@sjsu.edu>
2019-02-08 00:21:10 -0800

    Final commit for Lab1
    Signed-off-by: Priya Chaitanya Yadav <priyachaitanya.yadav@sjsu.edu>

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -8,8 +8,8 @@ public abstract class GumballMachine
 {
     
     protected int num_gumballs;   
-        
-    public void insertQuarter(int coin){};
-    public void turnCrank(){};
+    
+    public abstract void insertQuarter(int coin);
+    public abstract void turnCrank();
     
 }
